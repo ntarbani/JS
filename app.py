47fbe12b98304badb53a645bea183ffbc1b47d91
Nitesh <ntarbani@gmail.com>
2021-06-29 18:20:19 +0530

    Combined Project

--- app.py
+++ app.py
@@ -5,10 +5,15 @@ from flask import Flask, render_template,request,redirect
 app = Flask(__name__)
 
 @app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/timezones')
 def index():
     return render_template('index.html')
-
-
+@app.route('/todolist')
+def todolist():
+    return render_template('todolist.html')
 
 
 if __name__=="__main__":
